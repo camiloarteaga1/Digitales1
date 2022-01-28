06da78270b2e41d81ff4904606cde1fa58058a3f
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2022-01-28 00:25:32 -0500

    First step
    Project creation and some settings

---
+++ Practica 2/Part2/Part2.srcs/sources_1/new/fun_tb.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 01/28/2022 12:22:21 AM
+-- Design Name: 
+-- Module Name: fun_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity fun_tb is
+    Port ( input : in STD_LOGIC_VECTOR (3 downto 0);
+           output : out STD_LOGIC_VECTOR (2 downto 0));
+end fun_tb;
+
+architecture Behavioral of fun_tb is
+
+begin
+
+
+end Behavioral;
