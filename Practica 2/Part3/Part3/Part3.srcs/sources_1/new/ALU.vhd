ccc8b1092cd6bc1896bcae01a169d5c0a43ea158
EasierMonk95 <popesinho@gmail.com>
2022-02-04 16:36:27 -0500

    Creation of the project for Part3

---
+++ Practica 2/Part3/Part3/Part3.srcs/sources_1/new/ALU.vhd
@@ -0,0 +1,32 @@
+----------------------------------------------------------------------------------
+-- Company: Universidad de Antioquia
+-- Engineer: Simon Sanchez Rua - Juan Camilo Arteaga Ibarra
+-- 
+-- Design Name: Practica 2
+-- Module Name: Parte 3 (ALU)
+-- Project Name: Diseno e implementacion de circuitos combinacionales 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ALU is
+--  Port ( );
+end ALU;
+
+architecture Behavioral of ALU is
+
+begin
+
+
+end Behavioral;
