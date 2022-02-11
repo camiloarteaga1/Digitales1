6db84f03aad0fac0d4aec967d1340be3cbcbb491
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2022-02-11 17:55:34 -0500

    Baby step
    Initializing project.

---
+++ Practica 3/Practica3/Practica3.srcs/sources_1/new/Circuit.vhd
@@ -0,0 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company: Universidad de Antioquia
+-- Engineer: Simon Sanchez Rua - Juan Camilo Arteaga Ibarra
+-- 
+-- Create Date: 02/11/2022 05:24:01 PM
+-- Design Name: Diseño e implemetacion de circuitos combinacionales modulare
+-- Module Name: Circuit - Behavioral
+-- Project Name: Practica 3
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+use STD.textio.all;
+use IEEE.std_logic_textio.all;
+
+library UNISIM;
+use UNISIM.VCOMPONENTS.all;
+
+entity Circuit is
+  Port (
+        Add_A : in STD_LOGIC_VECTOR(2 downto 0);
+        Add_B : in STD_LOGIC_VECTOR(2 downto 0);
+        DataA : in STD_LOGIC_VECTOR(3 downto 0);
+        DataB : in STD_LOGIC_VECTOR(3 downto 0);
+        FA : in STD_LOGIC;
+        FB : in STD_LOGIC;
+        en : in STD_LOGIC_VECTOR(0 to 2);
+        Sel_ALU : in STD_LOGIC_VECTOR(2 downto 0);
+        BA : out STD_LOGIC_VECTOR(3 downto 0);
+        BB : out STD_LOGIC_VECTOR(3 downto 0);
+        S : out STD_LOGIC_VECTOR(4 downto 0); --Pos 4, carry out
+        Resultado : out STD_LOGIC_VECTOR(3 downto 0);
+        Salida : out STD_LOGIC_VECTOR(6 downto 0);
+        clk : in STD_LOGIC
+  );
+end Circuit;
+
+architecture Behavioral of Circuit is
+
+begin
+
+
+end Behavioral;
