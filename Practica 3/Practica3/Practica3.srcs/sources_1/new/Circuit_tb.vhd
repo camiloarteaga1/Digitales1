e5f4e533e12d6f47f48cdd67ad7b61bda3e44cd5
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2022-02-23 16:03:46 -0500

    Starting Test Bench

--- Practica 3/Practica3/Practica3.srcs/sources_1/new/Circuit_tb.vhd
+++ Practica 3/Practica3/Practica3.srcs/sources_1/new/Circuit_tb.vhd
@@ -1,43 +1,112 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: UdeA
+-- Engineer: Simon Sanchez Rua - Juan Camilo Arteaga Ibarra
 -- 
--- Create Date: 02/11/2022 05:24:01 PM
--- Design Name: 
+-- Create Date: 2/23/2022
+-- Design Name: Circuit_tb
 -- Module Name: Circuit_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
+-- Project Name: Practica 3
+-- Target Devices: Basys 3
 -- 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use STD.textio.all;
+use IEEE.std_logic_textio.all;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library UNISIM;
+use UNISIM.VCOMPONENTS.all;
 
-entity Circuit_tb is
---  Port ( );
+Entity Circuit_tb Is
 end Circuit_tb;
 
-architecture Behavioral of Circuit_tb is
-
+Architecture behavior of Circuit_tb Is
+	Component Circuit
+        port (
+            Add_A : in STD_LOGIC_VECTOR(2 downto 0);
+            Add_B : in STD_LOGIC_VECTOR(2 downto 0);
+            DataA : in STD_LOGIC_VECTOR(3 downto 0);
+            DataB : in STD_LOGIC_VECTOR(3 downto 0);
+            FA : in STD_LOGIC;
+            FB : in STD_LOGIC;
+            en : in STD_LOGIC_VECTOR(0 to 2);
+            Sel_ALU : in STD_LOGIC_VECTOR(2 downto 0);
+            Salida : out STD_LOGIC_VECTOR(6 downto 0);
+            clk : in STD_LOGIC;
+            carry : out STD_LOGIC
+            );	
+    end Component;
+        
+    Signal add_a : STD_LOGIC_VECTOR(2 downto 0);
+    Signal add_b : STD_LOGIC_VECTOR(2 downto 0);
+    Signal dataa : STD_LOGIC_VECTOR(3 downto 0);
+    Signal datab : STD_LOGIC_VECTOR(3 downto 0);
+    Signal fa : STD_LOGIC;
+    Signal fb : STD_LOGIC;
+    Signal ena : STD_LOGIC_VECTOR(0 to 2);
+    Signal sel_ALU : STD_LOGIC_VECTOR(2 downto 0);
+    Signal salida : STD_LOGIC_VECTOR(6 downto 0);
+    Signal CLK : STD_LOGIC;
+    Signal Carry : STD_LOGIC;
+          
+    --RomA           
+    procedure RomA(
+        addrs : in std_logic_vector(2 downto 0);
+        data : out std_logic_vector(3 downto 0)
+    ) is
+        
+        type rom_type is array (0 to 7) of std_logic_vector (3 downto 0);
+        constant ROM : rom_type :=(x"7",x"6",x"5",x"3",x"1",x"F",x"D",x"A");
+        
+        begin
+            data := ROM(conv_integer(addrs));
+        
+    end RomA;
+    
+    --RomB
+    procedure RomB(
+        addrs : in std_logic_vector(2 downto 0);
+        data : out std_logic_vector(3 downto 0)
+    ) is
+        
+        type rom_type is array (0 to 7) of std_logic_vector (3 downto 0);
+        constant ROM : rom_type :=(x"5",x"7",x"3",x"8",x"2",x"9",x"1",x"A");
+        
+        begin
+            data := ROM(conv_integer(addrs));
+        
+    end RomB;
+    
+    --Buffers
+    procedure Buffers(
+        inputData : in std_logic_vector(3 downto 0) := "0000";
+        inputRomB : in std_logic_vector(3 downto 0) := "0000";
+        outputBuffer : out std_logic_vector(3 downto 0)
+    )is
+    
+    variable expectedOutput : std_logic_vector(3 downto 0);
+            
+    begin
+   
+        if not(fb)='1' then
+            expectedOutput := inputData;
+            
+        elsif not(not(fb)) = '1' then
+            expectedOutput := inputRomB;
+        
+        else
+            expectedOutput := "ZZZZ";
+        end if;
+        
+        outputBuffer := expectedOutput;
+    End Buffers;
+            
+    
+            
 begin
-
-
-end Behavioral;
+end behavior;
