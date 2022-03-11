49f69b206455f5602a856340942a9a20c2b6f02c
camiloarteaga1 <camilo.arteaga1@udea.edu.co>
2022-03-11 17:06:54 -0500

    Project Creation
    Starting animations

---
+++ Practica 4/Practica4/Practica4.srcs/sources_1/new/Animation.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/11/2022 04:13:07 PM
+-- Design Name: 
+-- Module Name: Animation - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Animation is
+--  Port ( );
+end Animation;
+
+architecture Behavioral of Animation is
+
+begin
+
+
+end Behavioral;
